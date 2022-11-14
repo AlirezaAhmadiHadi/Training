7da29b945de40d5f85fd60d52d1175c46e219f2b
Alireza <Alireza.hadi1308.aah@gmail.com>
2022-11-14 14:18:04 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,243 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "navigation test",
+      home: HomePage(),
+    );
+  }
+}
+
+// gitIgnore
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(builder: (context, child) {
+      return Scaffold(
+          backgroundColor: const Color.fromARGB(255, 10, 14, 49),
+          appBar: AppBar(
+            title: const Text("Training..."),
+          ),
+          body: Container(
+            padding:
+                const EdgeInsets.only(top: 20, bottom: 10, left: 10, right: 15),
+            child: Directionality(
+              textDirection: TextDirection.rtl,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  const Text("تنظیمات", style: TextStyle(color: Colors.white)),
+                  const Padding(padding: EdgeInsets.only(bottom: 15)),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: const <Widget>[
+                        Icon(
+                          FontAwesomeIcons.circleUser,
+                          size: 40,
+                          color: Colors.white,
+                        ),
+                        Padding(padding: EdgeInsets.only(left: 10)),
+                        Text("سعید کاشانی",
+                            style: TextStyle(color: Colors.white)),
+                      ]),
+                  const Padding(padding: EdgeInsets.only(top: 10)),
+                  Container(
+                    height: 30,
+                    decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(15)),
+                    child: const Center(
+                      child: Text(
+                        "خرید اشتراک",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  Center(
+                      child: Container(
+                          margin: const EdgeInsets.only(top: 50),
+                          child: ElevatedButton(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => const TablePage()),
+                                );
+                              },
+                              child: const Text("Table Page"))))
+                ],
+              ),
+            ),
+          ));
+    });
+  }
+}
+
+class TablePage extends StatelessWidget {
+  const TablePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        home: Directionality(
+            textDirection: TextDirection.rtl,
+            child: Scaffold(
+                backgroundColor: const Color.fromARGB(255, 10, 14, 49),
+                appBar: AppBar(
+                  title: const Text('سابقه خرید'),
+                  leading: (IconButton(
+                    icon: const Icon(Icons.navigate_before),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  )),
+                ),
+                body: Directionality(
+                  textDirection: TextDirection.rtl,
+                  child: Center(
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(top: 30),
+                          child: DataTable(
+                            headingRowColor: MaterialStateColor.resolveWith(
+                                (states) => const Color.fromARGB(255, 139, 133, 133)),
+                            columns: const [
+                              DataColumn(
+                                  label: Expanded(
+                                      child: Text('عنوان',
+                                          textAlign: TextAlign.center,
+                                          style: TextStyle(
+                                            fontSize: 18,
+                                            color: Colors.white,
+                                          )))),
+                              DataColumn(
+                                  label: Expanded(
+                                      child: Text('تاریخ خرید',
+                                          style: TextStyle(
+                                            fontSize: 18,
+                                            color: Colors.white,
+                                          )))),
+                              DataColumn(
+                                  label: Expanded(
+                                      child: Text('قیمت',
+                                          style: TextStyle(
+                                              fontSize: 18,
+                                              color: Colors.white)))),
+                              DataColumn(
+                                  label: Expanded(
+                                      child: Text("جزییات",
+                                          textAlign: TextAlign.center,
+                                          style: TextStyle(
+                                            fontSize: 18,
+                                            color: Colors.white,
+                                          ))))
+                            ],
+                            rows: [
+                              DataRow(cells: [
+                                DataCell(Center(
+                                    child: Row(
+                                  children: const [
+                                    Padding(
+                                      padding: EdgeInsets.only(left: 10),
+                                      child: Icon(
+                                        Icons.beenhere_outlined,
+                                        color: Colors.red,
+                                        size: 18.0,
+                                      ),
+                                    ),
+                                    Text("یک روزه",
+                                        style: TextStyle(color: Colors.white))
+                                  ],
+                                ))),
+                                const DataCell(Center(
+                                  child: Text("۰۰/۱۱/۲۱",
+                                      style: TextStyle(color: Colors.white)),
+                                )),
+                                const DataCell(Center(
+                                  child: Text("۱۰/۹۰۰",
+                                      style: TextStyle(color: Colors.white)),
+                                )),
+                                DataCell(Center(
+                                    child: IconButton(
+                                        onPressed: () {},
+                                        icon: const Icon(Icons.navigate_next,
+                                            color: Colors.white)))),
+                              ]),
+                              DataRow(cells: [
+                                DataCell(Center(
+                                    child: Row(
+                                  children: const [
+                                    Padding(
+                                      padding: EdgeInsets.only(left: 10),
+                                      child: Icon(
+                                        Icons.beenhere_outlined,
+                                        color: Colors.red,
+                                        size: 18.0,
+                                      ),
+                                    ),
+                                    Text("یک روزه",
+                                        style: TextStyle(color: Colors.white))
+                                  ],
+                                ))),
+                                const DataCell(Center(
+                                  child: Text("۰۰/۱۱/۲۱",
+                                      style: TextStyle(color: Colors.white)),
+                                )),
+                                const DataCell(Center(
+                                  child: Text("۱۰/۹۰۰",
+                                      style: TextStyle(color: Colors.white)),
+                                )),
+                                DataCell(Center(
+                                    child: IconButton(
+                                        onPressed: () {},
+                                        icon: const Icon(Icons.navigate_next,
+                                            color: Colors.white)))),
+                              ]),
+                              DataRow(cells: [
+                                DataCell(Center(
+                                    child: Row(
+                                  children: const [
+                                    Padding(
+                                      padding: EdgeInsets.only(left: 10),
+                                      child: Icon(
+                                        Icons.beenhere_outlined,
+                                        color: Colors.pink,
+                                        size: 18.0,
+                                      ),
+                                    ),
+                                    Text("سه ماهه",
+                                        style: TextStyle(color: Colors.white))
+                                  ],
+                                ))),
+                                const DataCell(Center(
+                                  child: Text("۰۰/۱۱/۲۱",
+                                      style: TextStyle(color: Colors.white)),
+                                )),
+                                const DataCell(Center(
+                                  child: Text("۱۰/۹۰۰",
+                                      style: TextStyle(color: Colors.white)),
+                                )),
+                                DataCell(Center(
+                                    child: IconButton(
+                                        onPressed: () {},
+                                        icon: const Icon(Icons.navigate_next,
+                                            color: Colors.white)))),
+                              ]),
+                            ],
+                          ),
+                        )
+                      ])),
+                ))));
   }
 }
