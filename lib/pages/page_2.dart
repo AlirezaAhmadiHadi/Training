e30f3649a4859566f6c9d4f29c045fe4a71d3ea3
Alireza <Alireza.hadi1308.aah@gmail.com>
2022-11-17 02:03:55 +0330

    project 2.2.1

--- lib/pages/page_2.dart
+++ lib/pages/page_2.dart
@@ -20,136 +20,163 @@ class Page2 extends StatelessWidget {
         body: Directionality(
           textDirection: TextDirection.rtl,
           child: Center(
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(top: 30),
-                  child: DataTable(
-                    headingRowColor: MaterialStateColor.resolveWith(
-                        (states) => const Color.fromARGB(255, 139, 133, 133)),
-                    columns: const [
-                      DataColumn(
-                          label: Expanded(
-                              child: Text('عنوان',
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    color: Colors.white,
-                                  )))),
-                      DataColumn(
-                          label: Expanded(
-                              child: Text('تاریخ خرید',
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    color: Colors.white,
-                                  )))),
-                      DataColumn(
-                          label: Expanded(
-                              child: Text('قیمت',
-                                  style: TextStyle(
-                                      fontSize: 18, color: Colors.white)))),
-                      DataColumn(
-                          label: Expanded(
-                              child: Text("جزییات",
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    color: Colors.white,
-                                  ))))
-                    ],
-                    rows: [
-                      DataRow(cells: [
-                        DataCell(Row(
-                          children: const [
-                            Padding(
-                              padding: EdgeInsets.only(left: 10),
-                              child: Icon(
-                                Icons.beenhere_outlined,
-                                color: Colors.red,
-                                size: 18.0,
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(top: 30),
+                    child: DataTable(
+                      headingRowColor: MaterialStateColor.resolveWith(
+                          (states) => const Color.fromARGB(255, 139, 133, 133)),
+                      columns: const [
+                        DataColumn(
+                            label: Expanded(
+                                child: Text('عنوان',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      color: Colors.white,
+                                    )))),
+                        DataColumn(
+                            label: Expanded(
+                                child: Text('تاریخ خرید',
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      color: Colors.white,
+                                    )))),
+                        DataColumn(
+                            label: Expanded(
+                                child: Text('قیمت',
+                                    style: TextStyle(
+                                        fontSize: 18, color: Colors.white)))),
+                        DataColumn(
+                            label: Expanded(
+                                child: Text("جزییات",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      color: Colors.white,
+                                    ))))
+                      ],
+                      rows: [
+                        DataRow(cells: [
+                          DataCell(
+                            Row(
+                              children: const [
+                                Padding(
+                                  padding: EdgeInsets.only(left: 10),
+                                  child: Icon(
+                                    Icons.beenhere_outlined,
+                                    color: Colors.red,
+                                    size: 18.0,
+                                  ),
+                                ),
+                                Text("یک روزه",
+                                    style: TextStyle(color: Colors.white))
+                              ],
+                            ),
+                          ),
+                          const DataCell(
+                            Center(
+                              child: Text("۰۰/۱۱/۲۱",
+                                  style: TextStyle(color: Colors.white)),
+                            ),
+                          ),
+                          const DataCell(
+                            Center(
+                              child: Text("۱۰/۹۰۰",
+                                  style: TextStyle(color: Colors.white)),
+                            ),
+                          ),
+                          DataCell(Center(
+                            child: IconButton(
+                              onPressed: () {},
+                              icon: const Icon(
+                                Icons.navigate_next,
+                                color: Colors.white,
                               ),
                             ),
-                            Text("یک روزه",
-                                style: TextStyle(color: Colors.white))
-                          ],
-                        )),
-                        const DataCell(Center(
-                          child: Text("۰۰/۱۱/۲۱",
-                              style: TextStyle(color: Colors.white)),
-                        )),
-                        const DataCell(Center(
-                          child: Text("۱۰/۹۰۰",
-                              style: TextStyle(color: Colors.white)),
-                        )),
-                        DataCell(Center(
+                          )),
+                        ]),
+                        DataRow(cells: [
+                          DataCell(Row(
+                            children: const [
+                              Padding(
+                                padding: EdgeInsets.only(left: 10),
+                                child: Icon(
+                                  Icons.beenhere_outlined,
+                                  color: Colors.red,
+                                  size: 18.0,
+                                ),
+                              ),
+                              Text("یک روزه",
+                                  style: TextStyle(color: Colors.white))
+                            ],
+                          )),
+                          const DataCell(Center(
+                            child: Text("۰۰/۱۱/۲۱",
+                                style: TextStyle(color: Colors.white)),
+                          )),
+                          const DataCell(Center(
+                            child: Text("۱۰/۹۰۰",
+                                style: TextStyle(color: Colors.white)),
+                          )),
+                          DataCell(Center(
                             child: IconButton(
-                                onPressed: () {},
-                                icon: const Icon(Icons.navigate_next,
-                                    color: Colors.white)))),
-                      ]),
-                      DataRow(cells: [
-                        DataCell(Row(
-                          children: const [
-                            Padding(
-                              padding: EdgeInsets.only(left: 10),
-                              child: Icon(
-                                Icons.beenhere_outlined,
-                                color: Colors.red,
-                                size: 18.0,
+                              onPressed: () {},
+                              icon: const Icon(
+                                Icons.navigate_next,
+                                color: Colors.white,
                               ),
                             ),
-                            Text("یک روزه",
-                                style: TextStyle(color: Colors.white))
-                          ],
-                        )),
-                        const DataCell(Center(
-                          child: Text("۰۰/۱۱/۲۱",
-                              style: TextStyle(color: Colors.white)),
-                        )),
-                        const DataCell(Center(
-                          child: Text("۱۰/۹۰۰",
-                              style: TextStyle(color: Colors.white)),
-                        )),
-                        DataCell(Center(
+                          )),
+                        ]),
+                        DataRow(cells: [
+                          DataCell(
+                            Row(
+                              children: const [
+                                Padding(
+                                  padding: EdgeInsets.only(left: 10),
+                                  child: Icon(
+                                    Icons.beenhere_outlined,
+                                    color: Colors.pink,
+                                    size: 18.0,
+                                  ),
+                                ),
+                                Text(
+                                  "سه ماهه",
+                                  style: TextStyle(color: Colors.white),
+                                )
+                              ],
+                            ),
+                          ),
+                          const DataCell(
+                            Center(
+                              child: Text("۰۰/۱۱/۲۱",
+                                  style: TextStyle(color: Colors.white)),
+                            ),
+                          ),
+                          const DataCell(
+                            Center(
+                              child: Text("۱۰/۹۰۰",
+                                  style: TextStyle(color: Colors.white)),
+                            ),
+                          ),
+                          DataCell(Center(
                             child: IconButton(
-                                onPressed: () {},
-                                icon: const Icon(Icons.navigate_next,
-                                    color: Colors.white)))),
-                      ]),
-                      DataRow(cells: [
-                        DataCell(Row(
-                          children: const [
-                            Padding(
-                              padding: EdgeInsets.only(left: 10),
-                              child: Icon(
-                                Icons.beenhere_outlined,
-                                color: Colors.pink,
-                                size: 18.0,
+                              onPressed: () {},
+                              icon: const Icon(
+                                Icons.navigate_next,
+                                color: Colors.white,
                               ),
                             ),
-                            Text("سه ماهه",
-                                style: TextStyle(color: Colors.white))
-                          ],
-                        )),
-                        const DataCell(Center(
-                          child: Text("۰۰/۱۱/۲۱",
-                              style: TextStyle(color: Colors.white)),
-                        )),
-                        const DataCell(Center(
-                          child: Text("۱۰/۹۰۰",
-                              style: TextStyle(color: Colors.white)),
-                        )),
-                        DataCell(Center(
-                            child: IconButton(
-                                onPressed: () {},
-                                icon: const Icon(Icons.navigate_next,
-                                    color: Colors.white)))),
-                      ]),
-                    ],
-                  ),
-                )
-              ])),
+                          )),
+                        ]),
+                      ],
+                    ),
+                  )
+                ]),
+          ),
         ),
       );
     });
