9ad14f1129d2b4db2d7385c5899f0af0d2d97df8
Alireza <Alireza.hadi1308.aah@gmail.com>
2022-11-18 02:11:08 +0330

    project 4.0.1

--- lib/pages/page_3.dart
+++ lib/pages/page_3.dart
@@ -10,192 +10,229 @@ class Page3 extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Page 3"),
       ),
-      body: Directionality(
-        textDirection: TextDirection.rtl,
-        child: Column(
-          children: [
-            Container(
-              margin: const EdgeInsets.only(top: 20),
-              // padding: const EdgeInsets.only(top: 20, right: 20, left: 20),
-              child: Row(
-                children: [
-                  IconButton(
+      body: Container(
+        margin: const EdgeInsets.only(top: 20, right: 20, left: 20),
+        child: Directionality(
+          textDirection: TextDirection.rtl,
+          child: Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(top: 20),
+                // padding: const EdgeInsets.only(top: 20, right: 20, left: 20),
+                child: Row(
+                  children: [
+                    IconButton(
                       onPressed: () {},
                       icon: const Icon(
                         Icons.navigate_before,
                         color: Colors.white,
-                      )),
-                  Container(
-                    margin: const EdgeInsets.only(right: 3),
-                    child: const Text(
-                      "سابقه خرید",
-                      style: TextStyle(fontSize: 18, color: Colors.white),
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(top: 20, right: 20, left: 20),
-              child: Container(
-                color: const Color.fromARGB(255, 102, 99, 99),
-                height: 40,
-                // width: double.infinity,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: const [
-                    Text('عنوان',
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Colors.white,
-                        )),
-                    Text('تاریخ خرید',
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Colors.white,
-                        )),
-                    Text('قیمت',
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Colors.white,
-                        )),
-                    Text('جزییات',
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Colors.white,
-                        )),
+                    Container(
+                      margin: const EdgeInsets.only(right: 3),
+                      child: const Text(
+                        "سابقه خرید",
+                        style: TextStyle(fontSize: 18, color: Colors.white),
+                      ),
+                    ),
                   ],
                 ),
               ),
-            ),
-            Container(
-              padding: const EdgeInsets.only(right: 3),
-              margin: const EdgeInsets.only(top: 10, right: 20, left: 20),
-              child: Row(
+              Container(
+                color: const Color.fromARGB(255, 102, 99, 99),
+                height: 40,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: const [
+                    Text(
+                      'عنوان',
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      'تاریخ خرید',
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      'قیمت',
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      'جزییات',
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.only(right: 3),
+                margin: const EdgeInsets.only(top: 10),
+                child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Row(children: const [
-                      Padding(
-                        padding: EdgeInsets.only(left: 4),
-                        child: Icon(
-                          Icons.beenhere_outlined,
-                          color: Colors.red,
-                          size: 16,
+                    Row(
+                      children: const [
+                        Padding(
+                          padding: EdgeInsets.only(left: 4),
+                          child: Icon(
+                            Icons.beenhere_outlined,
+                            color: Colors.red,
+                            size: 16,
+                          ),
                         ),
-                      ),
-                      Text("یک روزه",
+                        Text(
+                          "یک روزه",
                           style: TextStyle(
                             fontSize: 15,
                             color: Colors.white,
-                          ))
-                    ]),
-                    const Text('00/11/21',
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Colors.white,
-                        )),
-                    const Text('10/900',
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Colors.white,
-                        )),
+                          ),
+                        )
+                      ],
+                    ),
+                    const Text(
+                      '00/11/21',
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const Text(
+                      '10/900',
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.white,
+                      ),
+                    ),
                     Padding(
-                        padding: const EdgeInsets.only(left: 20),
-                        child: IconButton(
-                            onPressed: () {},
-                            icon: const Icon(Icons.navigate_next,
-                                color: Colors.white)))
-                  ]),
-            ),
-            Container(
-                margin: const EdgeInsets.only(left: 20, right: 20),
-                child: const Divider(
-                  color: Color.fromARGB(255, 71, 71, 71),
-                )),
-            Container(
-              padding: const EdgeInsets.only(right: 3),
-              margin: const EdgeInsets.only(top: 10, right: 20, left: 20),
-              child: Row(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.navigate_next,
+                            color: Colors.white),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              const Divider(
+                color: Color.fromARGB(255, 71, 71, 71),
+              ),
+              Container(
+                padding: const EdgeInsets.only(right: 3),
+                margin: const EdgeInsets.only(top: 10),
+                child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Row(children: const [
-                      Padding(
-                        padding: EdgeInsets.only(left: 4),
-                        child: Icon(
-                          Icons.beenhere_outlined,
-                          color: Colors.red,
-                          size: 16,
+                    Row(
+                      children: const [
+                        Padding(
+                          padding: EdgeInsets.only(left: 4),
+                          child: Icon(
+                            Icons.beenhere_outlined,
+                            color: Colors.red,
+                            size: 16,
+                          ),
                         ),
-                      ),
-                      Text("یک روزه",
+                        Text(
+                          "یک روزه",
                           style: TextStyle(
                             fontSize: 15,
                             color: Colors.white,
-                          ))
-                    ]),
-                    const Text('00/11/21',
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Colors.white,
-                        )),
-                    const Text('10/900',
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Colors.white,
-                        )),
+                          ),
+                        )
+                      ],
+                    ),
+                    const Text(
+                      '00/11/21',
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const Text(
+                      '10/900',
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.white,
+                      ),
+                    ),
                     Padding(
-                        padding: const EdgeInsets.only(left: 20),
-                        child: IconButton(
-                            onPressed: () {},
-                            icon: const Icon(Icons.navigate_next,
-                                color: Colors.white)))
-                  ]),
-            ),
-            Container(
-                margin: const EdgeInsets.only(left: 20, right: 20),
-                child: const Divider(
-                  color: Color.fromARGB(255, 71, 71, 71),
-                )),
-            Container(
-              padding: const EdgeInsets.only(right: 3),
-              margin: const EdgeInsets.only(top: 10, right: 20, left: 20),
-              child: Row(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.navigate_next,
+                            color: Colors.white),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              const Divider(
+                color: Color.fromARGB(255, 71, 71, 71),
+              ),
+              Container(
+                padding: const EdgeInsets.only(right: 3),
+                margin: const EdgeInsets.only(top: 10),
+                child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Row(children: const [
-                      Padding(
-                        padding: EdgeInsets.only(left: 4),
-                        child: Icon(
-                          Icons.beenhere_outlined,
-                          color: Colors.red,
-                          size: 16,
+                    Row(
+                      children: const [
+                        Padding(
+                          padding: EdgeInsets.only(left: 4),
+                          child: Icon(
+                            Icons.beenhere_outlined,
+                            color: Colors.red,
+                            size: 16,
+                          ),
                         ),
-                      ),
-                      Text("یک روزه",
+                        Text(
+                          "یک روزه",
                           style: TextStyle(
                             fontSize: 15,
                             color: Colors.white,
-                          ))
-                    ]),
-                    const Text('00/11/21',
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Colors.white,
-                        )),
-                    const Text('10/900',
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Colors.white,
-                        )),
+                          ),
+                        )
+                      ],
+                    ),
+                    const Text(
+                      '00/11/21',
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const Text(
+                      '10/900',
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.white,
+                      ),
+                    ),
                     Padding(
-                        padding: const EdgeInsets.only(left: 20),
-                        child: IconButton(
-                            onPressed: () {},
-                            icon: const Icon(Icons.navigate_next,
-                                color: Colors.white)))
-                  ]),
-            )
-          ],
+                      padding: const EdgeInsets.only(left: 20),
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.navigate_next,
+                            color: Colors.white),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
